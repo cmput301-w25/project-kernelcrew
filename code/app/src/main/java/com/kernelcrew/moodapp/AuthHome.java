cd300a1d0e262a33189743ba429b0374a1855e61
Aidan Olsen <a.olsen2024@icloud.com>
2025-02-22 01:13:19 -0700

    Setup basic navigation controller

---
+++ code/app/src/main/java/com/kernelcrew/moodapp/AuthHome.java
@@ -0,0 +1,19 @@
+package com.kernelcrew.moodapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AuthHome extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_auth_home, container, false);
+        return view;
+    }
+}
