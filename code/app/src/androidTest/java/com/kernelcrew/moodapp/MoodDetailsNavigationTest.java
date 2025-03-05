530c18605d21542e52d11c51fa28ff9989d14f6a
adityadipakpatel <adityadipakpatel@gmail.com>
2025-03-05 12:12:44 -0700

    some changes similar to home feed test

--- code/app/src/androidTest/java/com/kernelcrew/moodapp/MoodDetailsNavigationTest.java
+++ code/app/src/androidTest/java/com/kernelcrew/moodapp/MoodDetailsNavigationTest.java
@@ -1,7 +1,6 @@
 package com.kernelcrew.moodapp;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -11,34 +10,51 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 
 import android.os.SystemClock;
 
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.kernelcrew.moodapp.data.Emotion;
 import com.kernelcrew.moodapp.data.MoodEvent;
 import com.kernelcrew.moodapp.ui.MainActivity;
-import com.kernelcrew.moodapp.R;
 import com.kernelcrew.moodapp.ui.Mood;
 
-import org.junit.Before;
+import org.junit.After;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.ExecutionException;
+
 @RunWith(AndroidJUnit4.class)
 public class MoodDetailsNavigationTest extends FirebaseEmulatorMixin {
+    private static final String USER_EMAIL = "test@kernelcrew.com";
+    private static final String USER_PASSWORD = "Password@1234";
+    private static final String DATA_EMOTION = "Happy";
+    private static final String DATA_TRIGGER = "Morning Coffee";
+    private static final String DATA_SOCIALSITUATION = "With Friends";
+    private static final String DATA_REASON = "Celebration";
+    private static final String DATA_PHOTOURL = "https://example.com/photo.jpg";
+    private static final double DATA_LATITUDE = 34.052235;
+    private static final double DATA_LONGITUDE = -118.243683;
 
     @Rule
     public ActivityScenarioRule<MainActivity> activityScenarioRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
-    @Before
-    public void seedDatabase() {
+    @BeforeClass
+    public static void seedDatabase() throws ExecutionException, InterruptedException {
         // Seed Firestore with a test Mood and corresponding MoodEvent.
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        Tasks.await(auth.createUserWithEmailAndPassword(USER_EMAIL, USER_PASSWORD));
 
         // Seed a basic Mood document used in the HomeFeed RecyclerView.
         CollectionReference moodsRef = db.collection("moods");
@@ -50,36 +66,44 @@ public class MoodDetailsNavigationTest extends FirebaseEmulatorMixin {
         MoodEvent testEvent = new MoodEvent(
                 "dummyUser",
                 Emotion.HAPPINESS,
-                "Morning Coffee",       // trigger
-                "With Friends",         // socialSituation
-                "Celebration",          // reason
-                "https://example.com/photo.jpg", // photoUrl
-                34.052235,              // latitude
-                -118.243683             // longitude
+                DATA_TRIGGER,       // trigger
+                DATA_SOCIALSITUATION,         // socialSituation
+                DATA_REASON,          // reason
+                DATA_PHOTOURL, // photoUrl
+                DATA_LATITUDE,              // latitude
+                DATA_LONGITUDE             // longitude
         );
         // Set the id to match the test document id.
         testEvent.setId("testMoodId");
-        moodEventsRef.document("testMoodId").set(testEvent);
+        Tasks.await(moodEventsRef.document("testMoodId").set(testEvent));
 
-        // Wait a moment for data to be written.
-        SystemClock.sleep(2000);
+        auth.signOut();
     }
 
     @Test
     public void testNavigationToMoodDetails() throws InterruptedException {
         // On AuthHome screen: Click the "Sign In" button.
-        onView(withText("Sign In")).perform(click());
+        onView(withText("Sign In"))
+                .perform(click());
 
-        // On AuthSignIn screen: Fill in email and password.
+
+        // Now on AuthSignIn screen: Check that the email field is displayed.
         onView(withId(R.id.email))
                 .check(matches(isDisplayed()));
-        onView(withId(R.id.email))
-                .perform(replaceText("test@kernelcrew.com"), closeSoftKeyboard());
-        onView(withId(R.id.password))
-                .perform(replaceText("Password@1234"), closeSoftKeyboard());
-        onView(withId(R.id.signInButton)).perform(click());
 
-        // On HomeFeed screen: Verify that homeTextView is displayed.
+        // Fill in the email and password fields.
+        onView(withId(R.id.email))
+                .perform(replaceText(USER_EMAIL), ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.password))
+                .perform(replaceText(USER_PASSWORD), ViewActions.closeSoftKeyboard());
+
+        // Click the sign in button on AuthSignIn.
+        onView(withId(R.id.signInButton))
+                .perform(click());
+
+        SystemClock.sleep(1000);
+
+        // On HomeFeed screen: Verify that the homeTextView is displayed.
         onView(withId(R.id.homeTextView))
                 .check(matches(isDisplayed()));
 
@@ -92,18 +116,29 @@ public class MoodDetailsNavigationTest extends FirebaseEmulatorMixin {
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
         // Wait for the MoodDetails screen to load.
-        SystemClock.sleep(2000);
+        SystemClock.sleep(1000);
+
+        // Click on view details button to go to view details screen
+        onView(withId(R.id.viewDetailsButton))
+                .perform(click());
 
         // Verify that key elements on the MoodDetails screen are displayed and have the correct text.
         onView(withId(R.id.tvMoodState))
                 .check(matches(isDisplayed()));
+        SystemClock.sleep(3000);
         onView(withId(R.id.tvMoodState))
-                .check(matches(withText("Happy")));
+                .check(matches(withText(DATA_EMOTION)));
         onView(withId(R.id.tvTriggerValue))
-                .check(matches(withText("Morning Coffee")));
+                .check(matches(withText(DATA_TRIGGER)));
         onView(withId(R.id.tvSocialSituationValue))
-                .check(matches(withText("With Friends")));
+                .check(matches(withText(DATA_SOCIALSITUATION)));
         onView(withId(R.id.tvReasonValue))
-                .check(matches(withText("Celebration")));
+                .check(matches(withText(DATA_REASON)));
+    }
+
+    @After
+    public void signOutTheUser() {
+        // Not necessary right now, but will be if we add more tests
+        FirebaseAuth.getInstance().signOut();
     }
 }
