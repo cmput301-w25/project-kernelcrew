f0bfa52ae7760f993c0b1fea1df07be19681b70c
Aidan <46799759+PossiblyAShrub@users.noreply.github.com>
2025-03-28 00:01:10 -0600

    Hide private mood events from all other users (#238)
    * Hide private mood events from users other than their owners
    
    * Draw the mood event visibility on the mood item card (on home screen)
    
    * Add visibility icon to the mood details page
    
    * Fix failing test

--- code/app/src/main/java/com/kernelcrew/moodapp/data/MoodEventProvider.java
+++ code/app/src/main/java/com/kernelcrew/moodapp/data/MoodEventProvider.java
@@ -1,7 +1,5 @@
 package com.kernelcrew.moodapp.data;
 
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.Task;
@@ -9,10 +7,9 @@ import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.Filter;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.ListenerRegistration;
-import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Query;
 
 public class MoodEventProvider {
     private final CollectionReference collection;
@@ -37,10 +34,6 @@ public class MoodEventProvider {
         }
 
         return instance;
-    }
-
-    public static void setInstance(MoodEventProvider mockMoodEventProvider) {
-
     }
 
     /**
@@ -99,48 +92,15 @@ public class MoodEventProvider {
     }
 
     /**
-     * Add a snapshot listener to the mood events collection
-     * @param listener Snapshot listener to add
-     */
-    public void addSnapshotListener(@NonNull EventListener<QuerySnapshot> listener) {
-        collection.addSnapshotListener(listener);
-    }
-
-    /**
-     * Get a collection of mood events from the DB.
-     * @return A collection of mood events
-     */
-    public Task<QuerySnapshot> getMoodEvents(){
-        return collection.get();
-    }
-
-    /**
-     * Returns the Firestore CollectionReference for filtering purposes.
-     * This reference can be then in filtering to build queries or perform Firestore operations,
-     * like adding snapshot listeners or inserting and updating documents.
+     * Returns all mood events which can then be further filtered.
      *
-     * @return the CollectionReference instance for the mood events.
+     * @return All (visible) mood events.
      */
-    public CollectionReference getCollectionReference() {
-        return collection;
-    }
-
-    /**
-     * Add a snapshot listener to the mood events collection, filtered by the current user's UID.
-     * This method returns a ListenerRegistration that can be used to remove the listener.
-     *
-     * @param listener Snapshot listener to add
-     * @return ListenerRegistration that can be used to remove the listener
-     */
-    public ListenerRegistration addUserFilteredSnapshotListener(@NonNull EventListener<QuerySnapshot> listener) {
+    public Query getAll() {
         FirebaseUser user = auth.getCurrentUser();
-        if (user != null) {
-            // Only listen for mood events belonging to the current user
-            return collection.whereEqualTo("uid", user.getUid())
-                    .addSnapshotListener(listener);
-        } else {
-            // If no user is logged in, listen to an empty query
-            return collection.limit(0).addSnapshotListener(listener);
-        }
+        return collection.where(Filter.or(
+                Filter.and(Filter.equalTo("uid", user.getUid()),
+                           Filter.equalTo("visibility", "PRIVATE")),
+                Filter.equalTo("visibility", "PUBLIC")));
     }
 }
